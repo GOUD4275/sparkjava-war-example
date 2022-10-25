d4e7c4275b94e4685731d468fe6bae2d1deb2e62
moola nareshgoud <gowd2009@gmail.com>
2022-10-25 22:38:55 +0530

    mention multiple IPs and updating

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "CICD-Pipeline");
+		get("/hello", (req, res) -> "multiple-IP's");
 	}
 }
